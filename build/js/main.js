03f2a79511337196c9b3a669ab7509aac8b7fec6
leonidryb <leonidrybalkin@yandex.ru>
2020-11-03 13:44:38 +0300

    Исправляет замечания наставника

---
+++ build/js/main.js
@@ -0,0 +1,103 @@
+'use strict';
+(function () {
+
+  var phoneInput = document.querySelector('#phone');
+  var slider = document.querySelector('.swiper-container');
+  var slider1 = document.querySelector('.swiper-container-1');
+  var tabsBtnList = document.querySelectorAll('.ticket__limit-button');
+  var anchor = document.querySelector('.header__link');
+
+  // Phone mask
+  var maskOptions = {
+    mask: '+{7}(000)000-00-00'
+  };
+  if (phoneInput) {
+    var mask = new IMask(phoneInput, maskOptions);
+  }
+
+  // Slider trainers
+  if (slider)  {
+    var mySwiper = new Swiper(slider, {
+      loop: true,
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+
+      breakpoints: {
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 30,
+          slidesPerGroup: 2,
+        },
+        1200: {
+          slidesPerView: 3,
+          spaceBetween: 40,
+          slidesPerGroup: 3,
+        },
+        1366: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+          slidesPerGroup: 3,
+        }
+      }
+    });
+  }
+
+  // Slider review
+  if (slider1) {
+    var mySwiper1 = new Swiper(slider1, {
+      loop: true,
+      slidesPerView: 1,
+
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+  }
+
+  // Ticket cards tab
+  if (tabsBtnList) {
+    tabsBtnList.forEach(function (button) {
+      button.addEventListener('click', function () {
+        var ticketsBtnActive = document.querySelector('.ticket__limit-button--active');
+        if (ticketsBtnActive) {
+          ticketsBtnActive.classList.remove('ticket__limit-button--active');
+          button.classList.add('ticket__limit-button--active');
+        }
+
+        var dataTab = Number(button.getAttribute('data-tab'));
+        var tabsList = document.querySelectorAll('.ticket__tab');
+
+        if (tabsList) {
+          for (var i = 0; i < tabsList.length; i++) {
+            if (dataTab === i) {
+              tabsList[i].classList.add('ticket__tab--show');
+              tabsList[i].classList.remove('ticket__tab--hide');
+            } else {
+              tabsList[i].classList.remove('ticket__tab--show');
+              tabsList[i].classList.add('ticket__tab--hide');
+            }
+          }
+        }
+      });
+    });
+  }
+
+  // Scroll
+  if (anchor) {
+    anchor.addEventListener('click', function (event) {
+      event.preventDefault();
+      var block = anchor.getAttribute('href');
+      document.querySelector('' + block).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    });
+  }
+  /* eslint-disable new-cap, no-undef, max-nested-callbacks, no-unused-vars */
+})();
